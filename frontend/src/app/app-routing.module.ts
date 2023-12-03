b1cd2053705143d66aec05c5735bf387423a6c73
Peter Kiraly <peter.kiral@gmail.com>
2023-12-03 14:31:05 +0100

    Robot page component added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { RobotPageComponent } from './components/pages/robot-page/robot-page.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'search/:searchTerm', component:HomeComponent},
-  {path:'tag/:tag', component:HomeComponent}
+  {path:'tag/:tag', component:HomeComponent},
+  {path:'robot/:id', component:RobotPageComponent}
 ];
 
 @NgModule({
--- frontend/src/app/components/pages/robot-page/robot-page.component.ts
+++ frontend/src/app/components/pages/robot-page/robot-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RobotService } from 'src/app/services/robot.service';
+import { Robot } from 'src/app/shared/models/Robot';
 
 @Component({
   selector: 'app-robot-page',
   templateUrl: './robot-page.component.html',
   styleUrls: ['./robot-page.component.css']
 })
-export class RobotPageComponent {
+export class RobotPageComponent implements OnInit{
+  
+  robot!:Robot;
+  constructor(activatedRoute:ActivatedRoute, robotService:RobotService){
+    activatedRoute.params.subscribe((params) => {
+      if(params.id)
+      this.robot = robotService.getRobotById(params.id);
+    })
+  }
+  
+  ngOnInit(): void {
+    
+  }
 
 }
